ae69ae2cfc97a3ba103fe7f5bc93124150c66716
k22036 <k22036kk@aitech.ac.jp>
2025-06-19 15:01:23 +0900

    feat: DebugNodeTestsでのスリープ処理を非同期に変更

--- swift-node-like-node-REDTests/NodeRED/Nodes/Core/Common/DebugNodeTests.swift
+++ swift-node-like-node-REDTests/NodeRED/Nodes/Core/Common/DebugNodeTests.swift
@@ -96,7 +96,7 @@ struct DebugNodeTests {
             debugNode.receive(msg: msg)
             
             // 0.5秒待機
-            Thread.sleep(forTimeInterval: 0.5)
+            try await Task.sleep(nanoseconds: UInt64(0.5) * 1_000_000_000)
             debugNode.terminate()
             #expect(debugNode.isRunning == false)
         } catch {
