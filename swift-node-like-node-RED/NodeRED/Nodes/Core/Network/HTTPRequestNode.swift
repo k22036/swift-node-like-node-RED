2e56a2c1661ef6b3ee1c175b44c998165c0000bc
k22036 <k22036kk@aitech.ac.jp>
2025-07-02 13:26:02 +0900

    fix: HTTPリクエストノードのContent-Typeヘッダー設定を改善

--- swift-node-like-node-RED/NodeRED/Nodes/Core/Network/HTTPRequestNode.swift
+++ swift-node-like-node-RED/NodeRED/Nodes/Core/Network/HTTPRequestNode.swift
@@ -94,24 +94,17 @@ final class HTTPRequestNode: Codable, Node {
                 var request = URLRequest(url: requestURL)
                 request.httpMethod = method
                 if paytoqs == "body" {
-                    let contentTypeSet = false
                     if let dataPayload = msg.payload as? Data {
-                        if !contentTypeSet {
-                            request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
-                        }
+                        request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
                         request.httpBody = dataPayload
                     } else if let dictPayload = msg.payload as? [String: Any] {
                         if let bodyData = try? JSONSerialization.data(withJSONObject: dictPayload) {
+                            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
                             request.httpBody = bodyData
-                            if !contentTypeSet {
-                                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                            }
                         }
                     } else if let payloadStr = msg.payload as? String {
+                        request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
                         request.httpBody = payloadStr.data(using: .utf8)
-                        if !contentTypeSet {
-                            request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-                        }
                     }
                 }
                 do {
