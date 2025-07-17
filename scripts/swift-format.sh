420a247bd549736c9428d76c4febe9917581584e
k22036 <k22036kk@aitech.ac.jp>
2025-07-17 13:38:31 +0900

    fix: swift-formatスクリプトで複数ディレクトリのフォーマットチェックを追加

--- scripts/swift-format.sh
+++ scripts/swift-format.sh
@@ -2,8 +2,9 @@
 set -e
 
 # フォーマットしたいフォルダを指定
-TARGET_DIR="${SRCROOT:-.}"
+TARGET_DIRS=("${SRCROOT:-.}/swift-node-like-node-RED" "${SRCROOT:-.}/swift-node-like-node-REDTests" "${SRCROOT:-.}/swift-node-like-node-REDUITests")
 # swift-formatの設定ファイルを指定
+TARGET_DIR="${SRCROOT:-.}"
 config_file="$TARGET_DIR/.swift-format"
 
 # swift-format が存在するか
@@ -14,19 +15,19 @@ fi
 
 # GitHub Actions 上ではフォーマットのチェックのみ行い、差分があればエラーを返す
 if [ -n "$GITHUB_ACTIONS" ]; then
-    # format前とformat後で差分があるか確認
     echo "Checking diffs before and after swift-format ..."
 
     exit_code=0
-    while IFS= read -r file; do
-        if diff_output=$(diff -u "$file" <(xcrun swift-format format "$file" --configuration "$config_file")); then
-            continue
-        else
-            echo "Difference found in $file:"
-            # echo "$diff_output"
-            exit_code=1
-        fi
-    done < <(find "$TARGET_DIR" -type f -name '*.swift')
+    for dir in "${TARGET_DIRS[@]}"; do
+        while IFS= read -r file; do
+            if diff_output=$(diff -u "$file" <(xcrun swift-format format "$file" --configuration "$config_file")); then
+                continue
+            else
+                echo "Difference found in $file:"
+                exit_code=1
+            fi
+        done < <(find "$dir" -type f -name '*.swift')
+    done
 
     if [ "$exit_code" -ne 0 ]; then
         echo "::error Swift files are not properly formatted. Run swift-format with --in-place to fix."
