db933a91916af56cd7f0fcff9e9c8bdc1f932d8d
k22036 <k22036kk@aitech.ac.jp>
2025-07-24 11:38:15 +0900

    refactor: add @MainActor attribute to setup and teardown methods for better concurrency handling

--- swift-node-like-node-REDUITests/swift_node_like_node_REDUITests.swift
+++ swift-node-like-node-REDUITests/swift_node_like_node_REDUITests.swift
@@ -7,8 +7,10 @@
 
 import XCTest
 
+@MainActor
 final class Swift_node_like_node_REDUITests: XCTestCase {
 
+    @MainActor
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -18,6 +20,7 @@ final class Swift_node_like_node_REDUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
+    @MainActor
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
