a4dd084bd3d0b0def403e55cf1d8c9cba143ac05
k22036 <k22036kk@aitech.ac.jp>
2025-08-16 14:34:44 +0900

    refactor: remove redundant @MainActor annotations in test classes

--- swift-node-like-node-REDUITests/swift_node_like_node_REDUITests.swift
+++ swift-node-like-node-REDUITests/swift_node_like_node_REDUITests.swift
@@ -7,10 +7,8 @@
 
 import XCTest
 
-@MainActor
 final class Swift_node_like_node_REDUITests: XCTestCase {
 
-    @MainActor
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -23,12 +21,10 @@ final class Swift_node_like_node_REDUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    @MainActor
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
